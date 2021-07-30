e73e2db27dfa0c409e9792f4302f8f86f63647d9
HNO234 <dave910602@gmail.com>
2021-07-31 00:06:04 +0800

    refine GUI

--- src/main/java/ghidra_win32/UIProvider.java
+++ src/main/java/ghidra_win32/UIProvider.java
@@ -1,5 +1,7 @@
 package ghidra_win32;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,24 +12,19 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JList;
-import javax.swing.JMenu;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import docking.ComponentProvider;
-import docking.action.DockingAction;
 import ghidra.framework.plugintool.Plugin;
-import ghidra.program.model.listing.Program;
-import ghidra.program.util.ProgramLocation;
 
 class UIProvider extends ComponentProvider {
 
 	private JPanel panel = new JPanel();
-	private JButton button;
 	private JTextField findTextField;
 	private JTextArea result;
 	private Win32Data database;
@@ -36,14 +33,12 @@ class UIProvider extends ComponentProvider {
 
 	public UIProvider(Plugin plugin, String owner) {
 		super(plugin.getTool(), owner, owner);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
 		database = new Win32Data();
 		buildPanel();
 	}
 
 	private void buildPanel() {
-		JPanel searchPanel = new JPanel();
-		searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.LINE_AXIS));
 		findTextField = new JTextField(30);
 		findTextField.getDocument().addDocumentListener(new DocumentListener() {
 			@Override
@@ -69,35 +64,27 @@ class UIProvider extends ComponentProvider {
 			}
 			
 		});
-		searchPanel.add(findTextField);	
-		button = new JButton("Search");
-		button.addActionListener(new java.awt.event.ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				candidateResults.setVisible(false);
-				queryWin32Data(findTextField.getText());
-			}
-		 });
-		searchPanel.add(button);
+		JPanel search = new JPanel();
+		search.add(findTextField);
 		
-		JPanel resultPanel = new JPanel();
-		resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.LINE_AXIS));
-		result = new JTextArea(10,40);
-		resultPanel.add(result);
-		
-		panel.add(searchPanel);
-		panel.add(candidateResults);
 		MouseListener mouseListener = new MouseAdapter() {
 		    public void mouseClicked(MouseEvent e) {
 		        if (e.getClickCount() == 2) {
-		        	System.out.println(candidateResults.getSelectedIndex());
 		           String selectedItem = candidateResults.getSelectedValue();
-		           findTextField.setText(selectedItem);
+		          queryWin32Data(selectedItem);
 		         }
 		    }
 		};
 		candidateResults.addMouseListener(mouseListener);
-		panel.add(resultPanel);
+		JScrollPane scrollPane = new JScrollPane(candidateResults);
+		
+		result = new JTextArea(10,40);
+		
+		JPanel endPanel = new JPanel(new GridLayout(1,0));
+        endPanel.add(scrollPane);
+        endPanel.add(result);
+		panel.add(search);
+		panel.add(endPanel);
 	}
 	
 	public void openWindowByClicking(String token) {
@@ -106,7 +93,7 @@ class UIProvider extends ComponentProvider {
 	}
 	
 	private void queryWin32Data(String token) {
-		//System.out.println(token + " " + database.contains(token));
+		result.setText(token);
 	}
 
 
