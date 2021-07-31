7708a78f48db54b6ff939417a2094dcca29453d5
XYFC128 <xyfc128@gmail.com>
2021-07-31 15:12:35 +0800

    analyzer: give up supporting decompiler

--- src/main/java/ghidra_win32/ghidra_win32Analyzer.java
+++ src/main/java/ghidra_win32/ghidra_win32Analyzer.java
@@ -16,11 +16,7 @@
 package ghidra_win32;
 
 import java.math.BigInteger;
-import java.sql.ResultSet;
 import java.util.*;
-
-import ghidra.app.decompiler.*;
-import ghidra.app.decompiler.parallel.*;
 
 import ghidra.app.cmd.equate.SetEquateCmd;
 import ghidra.app.emulator.EmulatorHelper;
@@ -36,15 +32,11 @@ import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.lang.OperandType;
 import ghidra.program.model.lang.Register;
 import ghidra.program.model.listing.*;
-import ghidra.program.model.pcode.HighFunction;
-import ghidra.program.model.pcode.PcodeOpAST;
-import ghidra.program.model.pcode.Varnode;
 import ghidra.program.model.symbol.*;
 import ghidra.program.util.*;
 import ghidra.program.util.SymbolicPropogator.Value;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
-import ghidra_win32.Win32Data.Constant;
 
 /**
  * TODO: Provide class-level documentation that describes what this analyzer does.
@@ -267,15 +259,12 @@ public class ghidra_win32Analyzer extends AbstractAnalyzer {
 		m_program = program;
 		m_monitor = monitor;
 		
-		ArrayList<Function> callers = new ArrayList<>();
-		
 		for(Function func : getFuncs()) {
 			ArrayList<Address> calls = getCalls(func);
 			for(Address call : calls) {
 				Function caller = m_program.getListing().getFunctionContaining(call);
 				if(caller == null)
 					continue;
-				callers.add(caller);
 				
 				System.out.println("Analyzing call at " + call + " " + func.getName() + ", caller : " + m_program.getListing().getFunctionContaining(call).getName());
 				for(int i = 0; i < func.getParameterCount(); i++) {
@@ -289,104 +278,9 @@ public class ghidra_win32Analyzer extends AbstractAnalyzer {
 			}
 		}
 		
-		try {
-			runDecompilerAnalysis(program, callers, monitor);
-		}
-		catch (Exception e) {
-			// Oh well.
-		}
-		
 		System.out.println("[Ghidra Win32 A] Analysis Compelete");
 		
 		return true;
-	}
-	
-	private void runDecompilerAnalysis(Program program, List<Function> functions,
-			TaskMonitor monitor) throws InterruptedException, Exception {
-
-		DecompileConfigurer configurer = decompiler -> setupDecompiler(program, decompiler);
-
-		DecompilerCallback<Void> callback = new DecompilerCallback<Void>(program, configurer) {
-
-			@Override
-			public Void process(DecompileResults results, TaskMonitor m) throws Exception {
-				inspectFunction(program, results, monitor);
-				return null;
-			}
-		};
-
-		try {
-			ParallelDecompiler.decompileFunctions(callback, functions, monitor);
-		}
-		finally {
-			callback.dispose();
-		}
-	}
-	
-	private void inspectFunction(Program program, DecompileResults results, TaskMonitor monitor) {
-		// For debug only
-		if(!results.getFunction().getName().equals("main"))
-			return;
-		/*
-		ArrayList<ClangNode> tokens = new ArrayList<>();
-		results.getCCodeMarkup().flatten(tokens);
-		System.out.println(results.getFunction().getName() + ":" + tokens);
-		for(ClangNode tok : tokens) {
-			if(tok instanceof ClangFieldToken) {
-				System.out.println(tok + ", NumChildren: " + tok.numChildren());
-				
-			}
-		}
-		System.out.println("=======");
-		LinkedList<ClangNode> q = new LinkedList<>();
-		q.add(results.getCCodeMarkup());
-		while(!q.isEmpty()) {
-			ClangNode tok = q.getFirst();q.removeFirst();
-			if(tok instanceof ClangVariableToken) {
-				Address addr = ((ClangVariableToken)tok).getVarnode().getAddress();
-				System.out.println("ClangVariableToken: " + tok + " Address:" + addr + ", parent:" + tok.Parent());
-			}
-			if(tok instanceof ClangStatement)
-				System.out.println("ClangStatement: " + tok + " Address:" + tok.getMinAddress());
-			if(tok.numChildren() > 0) {
-				for(int i = 0; i < tok.numChildren(); i++) {
-					q.addLast(tok.Child(i));
-				}
-			}
-		}
-		
-		*/
-		
-		HighFunction highFunction = results.getHighFunction();
-		if (highFunction == null) {
-			return;
-		}
-		Function function = results.getFunction();
-		Iterator<PcodeOpAST> pcodeOps = highFunction.getPcodeOps();
-		while (pcodeOps.hasNext()) {
-
-			PcodeOpAST op = pcodeOps.next();
-			String mnemonic = op.getMnemonic();
-			if(mnemonic.equals("CALL")) {
-				System.out.println(mnemonic + ":");
-				System.out.println("Inputs: ");
-				for(Varnode var : op.getInputs()) {
-					System.out.println(var.getOffset());
-				}
-				System.out.println("Outputs: " + op.getOutput());
-			}
-		}
-		
-	}
-	
-	private void setupDecompiler(Program p, DecompInterface decompiler) {
-		decompiler.toggleCCode(true);
-		decompiler.toggleSyntaxTree(true);
-		decompiler.setSimplificationStyle("decompile");
-		DecompileOptions options = new DecompileOptions();
-		options.grabFromProgram(p);
-		options.setEliminateUnreachable(false);
-		decompiler.setOptions(options);
 	}
 
 }
