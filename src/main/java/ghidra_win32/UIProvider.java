34791ca687960e90b03bd731e2b48a8af2e2595e
XYFC128 <xyfc128@gmail.com>
2021-07-31 19:57:23 +0800

    fix open url on linux

--- src/main/java/ghidra_win32/UIProvider.java
+++ src/main/java/ghidra_win32/UIProvider.java
@@ -32,6 +32,7 @@ import org.python.icu.impl.number.Endpoint;
 
 import docking.ComponentProvider;
 import ghidra.framework.plugintool.Plugin;
+import ghidra.util.Msg;
 import ghidra_win32.Win32Data.Constant;
 
 class UIProvider extends ComponentProvider {
@@ -51,6 +52,44 @@ class UIProvider extends ComponentProvider {
 		result.setEditorKit(JEditorPane.createEditorKitForContentType("text/html"));
 		result.setEditable(false);
 		buildPanel();
+	}
+	
+	private void openLink(String url) {
+	    String os = System.getProperty("os.name").toLowerCase();
+	    Runtime rt = Runtime.getRuntime();
+	    
+	    try{
+
+	        if (os.indexOf( "win" ) >= 0) {
+
+	            // this doesn't support showing urls in the form of "page.html#nameLink" 
+	            rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+
+	        } else if (os.indexOf( "mac" ) >= 0) {
+
+	            rt.exec( "open " + url);
+
+	            } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+
+	            // Do a best guess on unix until we get a platform independent way
+	            // Build a list of browsers to try, in this order.
+	            String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+	           			             "netscape","opera","links","lynx"};
+	            	
+	            // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+	            StringBuffer cmd = new StringBuffer();
+	            for (int i=0; i<browsers.length; i++)
+	                cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+	            	
+	            rt.exec(new String[] { "sh", "-c", cmd.toString() });
+
+	           } else {
+	                return;
+	           }
+	       }catch (Exception e){
+	        return;
+	       }
+	      return;	
 	}
 
 	private void buildPanel() {
@@ -82,7 +121,7 @@ class UIProvider extends ComponentProvider {
 		
 		MouseListener mouseListener = new MouseAdapter() {
 		    public void mouseClicked(MouseEvent e) {
-		        if (e.getClickCount() == 2) {
+		        if (e.getClickCount() == 1) {
 		           String selectedItem = candidateResults.getSelectedValue();
 		          queryWin32Data(selectedItem);
 		         }
@@ -93,15 +132,7 @@ class UIProvider extends ComponentProvider {
 		result.addHyperlinkListener(new HyperlinkListener() {
 		    public void hyperlinkUpdate(HyperlinkEvent e) {
 		        if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-		        	Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-		        	if (desktop==null)  return;
-		        	if(desktop.isDesktopSupported()) {
-		        		try {
-							desktop.getDesktop().browse(e.getURL().toURI());
-						} catch (IOException | URISyntaxException e1) {
-							e1.printStackTrace();
-						}
-			        }
+		        	openLink(e.getURL().toString());
 		        }
 		    }
 		});
